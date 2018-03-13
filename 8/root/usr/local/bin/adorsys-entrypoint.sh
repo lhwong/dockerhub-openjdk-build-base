1e1c7ea15b5984c516aa6aa36020b7056806adeb
Jan-Otto Kröpke <mail@jkroepke.de>
2018-03-13 17:49:24 +0100

    Use namespaces environment variables. (#7)
    * Use namespaces environment variables.

--- 8/root/usr/local/bin/adorsys-entrypoint.sh
+++ 8/root/usr/local/bin/adorsys-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # https://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
-if [ ! -z ${MIRROR_URL+x} ]; then
+if [ ! -z ${MAVEN_MIRROR_URL+x} ]; then
   envsubst < /usr/share/maven/ref/m2_settings.xml.template > /usr/share/maven/ref/settings.xml
 fi
 
